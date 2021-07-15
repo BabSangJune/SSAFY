5a1d2c7cec519a0b873a0d487e7a6e19d8d85afc
BabSangJune <applause1319@naver.com>
2021-07-15 13:38:35 +0900

    중간저장

---
+++ StartCamp/day02/data_type.py
@@ -0,0 +1,22 @@
+# 숫자
+number1 = 3      #정수
+number2 = 3.14   #실수
+number3 = 3 + 4j #복소수
+
+print(type(number1))
+print(type(number2))
+print(type(number3))
+
+# 문자
+string1 = '문자열'
+string2 = "문자열"
+print(type(string1))
+print(type(string2))
+
+# 참/거짓 (boolean)
+# 참/거짓 (Boolean)
+boolean1 = True
+boolean2 = False
+print(type(boolean1))
+print(type(boolean2))
+
