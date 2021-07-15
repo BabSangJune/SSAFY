91cf874c7bf0b427936200a9b9064b7bc9cf02e4
BabSangJune <applause1319@naver.com>
2021-07-15 11:47:46 +0900

    day01

--- StartCamp/day1/list_test.py
+++ StartCamp/day1/list_test.py
@@ -1,6 +1,7 @@
-      # 0  1  2  3  4  5  6  7  8
-dust = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      # 0 1 2 3 4 5 6 7           
+dust = [1,2,3,4,5,6,8,9]
 
-print(dust[0])
-print(dust[7])
-print(dust[0])
+
+print(dust[0]) #1
+print(dust[7]) #9
+# print(dust[8]) #?
