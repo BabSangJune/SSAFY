580ffd1d72fa0d9b1ea149bfba4a4ae892261059
BabSangJune <applause1319@naver.com>
2021-07-20 11:56:00 +0900

    SSAFY문서제외

--- test.py
+++ test.py
@@ -5,5 +5,10 @@
 n = int(input('가로 n : '))
 m = int(input('세로 m : '))
 
+# print((('*' * n) + '\n') * m)
+# # print('*' * n + '\n' + '*' * m) #이래서 안나옴
 
-print('*' * n + '\n' + '*' * m)
+for i in range(m): #세로(행) i 한번 반복시
+    for j in range(n): #가로(열) j 전체 반복복
+        print('*', end='')
+    print() # end 사용 후 print 문으로 출력
