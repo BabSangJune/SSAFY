a78a010d06732aba0e9196652dba7af6adbce2e7
BabSangJune <applause1319@naver.com>
2021-07-20 13:49:10 +0900

    2021.07.20과제제출

--- test.py
+++ test.py
@@ -2,13 +2,7 @@
 연습장
 """
 
-n = int(input('가로 n : '))
-m = int(input('세로 m : '))
+word = input()
 
-# print((('*' * n) + '\n') * m)
-# # print('*' * n + '\n' + '*' * m) #이래서 안나옴
-
-for i in range(m): #세로(행) i 한번 반복시
-    for j in range(n): #가로(열) j 전체 반복복
-        print('*', end='')
-    print() # end 사용 후 print 문으로 출력
+# 아래에 코드를 작성하시오.
+print(word[1:4:1])
