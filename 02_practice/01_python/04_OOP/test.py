cefba0af1a83d0520e5fb196fbe1a54772edf30c
BabSangJune <applause1319@naver.com>
2021-07-30 17:16:57 +0900

    2021.07.30_교재살짝더품

---
+++ 02_practice/01_python/04_OOP/test.py
@@ -0,0 +1,10 @@
+def dict_invert(my_dict):
+    answer = {}
+    for key, value in my_dict.items():
+        print(key, value)
+
+
+
+print(dict_invert({1: 10, 2: 20, 3: 30}))
+print(dict_invert({1: 10, 2: 20, 3: 30, 4: 30}))
+print(dict_invert({1: True, 2: True, 3: True}))
