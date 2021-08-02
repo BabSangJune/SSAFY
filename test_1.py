2150419353e4672436d25ca8a21bb07c7e3a6956
BabSangJune <applause1319@naver.com>
2021-08-02 12:38:38 +0900

    2021.08.02_폴더정리

--- test_1.py
+++ test_1.py
@@ -173,40 +173,8 @@ for key, value in st.items():
 #
 # print(a)
 
+a = [3, 2, 2, 3, 1]
 
-class Animal:
-    
-    def __init__(self, name):
-        self.name = name
+a.remove(3)
 
-    def walk(self):
-        print(f'{self.name}! 걷는다!')
-
-    def eat(self):
-        print(f'{self.name}! 먹는다!')
-
-
-class Dog(Animal):
-
-    def __init__(self, name):
-        super().__init__(name)
-
-    def bark(self):
-        print(f'{self.name}! 짖는다!!')
-
-class Bird(Animal):
-
-    def __init__(self, name):
-        super().__init__(name)
-
-    def fly(self):
-        print(f'{self.name}! 푸드덕!!')
-
-dog = Dog('멍멍이')
-dog.walk() #멍멍이! 달린다!
-dog.bark() #멍멍이! 짖늗다!
-
-bird = Bird('구구')
-bird.walk() #구구! 걷는다!
-bird.eat() #구구! 먹는다!
-bird.fly() #구구! 푸드덕!
+print(a)
